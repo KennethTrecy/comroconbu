d5f282a3c5dfa3475be1da3413b18ec3dcd4f490
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-19 22:56:00 +0800

    Improve the documentation of one method in abstract source file

--- src/abstract_source_file.js
+++ src/abstract_source_file.js
@@ -3,7 +3,23 @@
  */
 export default class AbstractSourceFile {
 	/**
-	 * Converts the representation into a configuration.
+	 * Converts the representation into an array of configurations.
+	 *
+	 * The array of configurations contains the configuration for the source itself and also the
+	 * external source(s) that must be bundled.
+	 *
+	 * @return {{
+	 *    "external": string[]
+	 *    "input": string,
+	 *    "output": {
+	 *       "file": string,
+	 *       "format": string,
+	 *       "globals": Object,
+	 *       "interop": string,
+	 *       "name": string
+	 *    },
+	 *    "plugins": any[]
+	 * }[]} An array of configurations. Note that `output.globals` and `external` are optional.
 	 */
 	toConfigurationArray() {
 		throw new TypeError("Cannot convert an abstract source to array of configurations.");
