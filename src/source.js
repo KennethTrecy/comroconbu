2303e2bc056eacb448fe85891288465baee1da53
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-19 22:01:10 +0800

    Add common info parameter to source

--- src/source.js
+++ src/source.js
@@ -1,9 +1,12 @@
+import CommonInfo from "./common_info";
+
 /**
  * Represents a source file that will be bundled.
  */
 export default class Source {
 	/**
 	 * Creates a representation of source.
+	 * @param {CommonInfo} commonInfo Common information for sources.
 	 * @param {string} name Name of the bundle.
 	 * @param {string} file The path of the source file to be bundled relative to the
 	 *                 `inputDirectory` of `CommonInfoBuilder` class.
@@ -11,7 +14,8 @@ export default class Source {
 	 * @param {Source[]} externals Optional. Array of external packages that will not be included in
 	 *                             the bundle.
 	 */
-	constructor(name, file, plugins, externals = []) {
+	constructor(commonInfo, name, file, plugins, externals = []) {
+		this._commonInfo = commonInfo;
 		this._name = name;
 		this._file = file;
 		this._plugins = plugins;
