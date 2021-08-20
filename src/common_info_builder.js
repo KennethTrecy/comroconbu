c9b17e1fdb384c7dfa29ba3d5cd7d1c9fcc8c39b
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-20 21:37:35 +0800

    Add other methods to configure other sources

--- src/common_info_builder.js
+++ src/common_info_builder.js
@@ -1,6 +1,8 @@
 import AbstractExternalPackage from "./abstract_external_package";
 import CommonInfo from "./common_info";
 import NamedSourceFile from "./source_file/named_source_file";
+import SourceDirectory from "./source_file/source_directory";
+import UnnamedSourceFile from "./source_file/unnamed_source_file";
 
 /**
  * Represents a builder which contains common information for configurations that will be created.
@@ -21,12 +23,38 @@ export default class CommonInfoBuilder {
 	 * @param {string} name Name of the bundle.
 	 * @param {string} file The path of the source file to be bundled relative to the
 	 *                 `inputDirectory` of this class.
-	 * @param {any} plugins Plugins that will be used to bundle the source file.
+	 * @param {any[]} plugins Plugins that will be used to bundle the source file.
 	 * @param {AbstractExternalPackage[]} externals Optional. Array of external packages that will
 	 *                                    not be included in the bundle.
-	 * @returns {NamedSourceFile} A representation of source.
+	 * @returns {NamedSourceFile} A representation of named source file.
 	 */
 	configureNamedSource(name, file, plugins, externals = []) {
 		return new NamedSourceFile(this._commonInfo, name, file, plugins, externals);
+	}
+
+	/**
+	 * Creates a representation of unnamed source file.
+	 * @param {string} file The path of the source file to be bundled relative to the
+	 *                 `inputDirectory` of this class.
+	 * @param {any[]} plugins Plugins that will be used to bundle the source file.
+	 * @param {AbstractExternalPackage[]} externals Optional. Array of external packages that will
+	 *                                    not be included in the bundle.
+	 * @returns {UnnamedSourceFile} A representation of unnamed source files.
+	 */
+	configureUnnamedSource(file, plugins, externals = []) {
+		return new UnnamedSourceFile(this._commonInfo, file, plugins, externals);
+	}
+
+	/**
+	 * Creates a representation of source directory.
+	 * @param {any[]} plugins Common plugins to be applied to the source files found in the input
+	 *                      directory.
+	 * @param {AbstractExternalPackage[]} externals Optional. Array of common external packages that
+	 *                                    will not be included in the configuration of each source
+	 *                                    file.
+	 * @returns {SourceDirectory} A representation of source directory.
+	 */
+	configureSourceDirectory(plugins, externals = []) {
+		return new SourceDirectory(this._commonInfo, plugins, externals);
 	}
 }
