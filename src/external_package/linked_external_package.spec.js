69b3a633acb1c6a58cdd63ce7714e29c3fee17fe
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-12-27 22:39:12 +0800

    test: move the test for linked external package

---
+++ src/external_package/linked_external_package.spec.js
@@ -0,0 +1,13 @@
+import { expect } from "chai"
+
+import { LinkedExternalPackage } from "../index"
+
+describe("Linked external package", () => {
+	it("can be converted to configuration array", () => {
+		const externalPackage = new LinkedExternalPackage("a", "b")
+
+		const configurations = externalPackage.toConfigurationArray()
+
+		expect(configurations).toEqual([])
+	})
+})
--- t/unit/external_package/linked_external_package.js
+++
@@ -1,9 +0,0 @@
-import { LinkedExternalPackage } from "../../../src";
-
-it("can be converted to configuration array", () => {
-	const externalPackage = new LinkedExternalPackage("a", "b");
-
-	const configurations = externalPackage.toConfigurationArray();
-
-	expect(configurations).toEqual([]);
-});
