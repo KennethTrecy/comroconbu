571932f413eebdb20eae9927bad2eb79d53d5825
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-12-27 15:16:09 +0800

    internal: clone contents to new file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export class Example {}
+import CommonInfoBuilder from "./common_info_builder";
+import ImportedExternalPackage from "./external_package/imported_external_package";
+import LinkedExternalPackage from "./external_package/linked_external_package";
+import NamedSourceFile from "./source_file/named_source_file";
+import RebundledExternalPackage from "./external_package/rebundled_external_package";
+import SourceDirectory from "./source_file/source_directory";
+import UnnamedSourceFile from "./source_file/unnamed_source_file";
+import interop from "./interop";
+
+export {
+	CommonInfoBuilder,
+	ImportedExternalPackage,
+	LinkedExternalPackage,
+	NamedSourceFile,
+	RebundledExternalPackage,
+	SourceDirectory,
+	UnnamedSourceFile,
+	interop
+};
