b60256c6b22a5913b4de2f3cbfc60648f9ca2773
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-12-27 14:58:59 +0800

    build: insert plugin to compile files

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,27 @@
-import { CommonInfoBuilder } from "./src/index";
+import { CommonInfoBuilder } from "./src/index"
+import esbuild from "rollup-plugin-esbuild-transform"
 
-const infoBuilder = new CommonInfoBuilder("src", "dist", "cjs");
+const infoBuilder = new CommonInfoBuilder("src", "dist", "cjs")
 
 export default [
 	infoBuilder.configureNamedSource(
 		"comcoronbu",
 		"index.js",
-		[],
+		[
+			esbuild([
+				{
+					"loader": "ts",
+					"tsconfig": "tsconfig.json"
+				},
+				{
+					"loader": "js",
+					"output": true
+				}
+			])
+		],
 		[
 			infoBuilder.linkExternalPackage("fs", "fs"),
 			infoBuilder.linkExternalPackage("path", "path")
 		]
 	).toOwnConfiguration()
-];
+]
