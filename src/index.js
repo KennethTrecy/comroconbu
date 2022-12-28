d00e8d0a9e8ee8930833731403a546f6c4e4dee6
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-12-28 18:23:05 +0800

    feat(source): expose relative path pair

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import CommonInfoBuilder from "./common_info_builder";
-import ImportedExternalPackage from "./external_package/imported_external_package";
-import LinkedExternalPackage from "./external_package/linked_external_package";
-import NamedSourceFile from "./source_file/named_source_file";
-import RebundledExternalPackage from "./external_package/rebundled_external_package";
-import SourceDirectory from "./source_file/source_directory";
-import UnnamedSourceFile from "./source_file/unnamed_source_file";
-import interop from "./interop";
+import CommonInfoBuilder from "./common_info_builder"
+import ImportedExternalPackage from "./external_package/imported_external_package"
+import LinkedExternalPackage from "./external_package/linked_external_package"
+import NamedSourceFile from "./source_file/named_source_file"
+import RebundledExternalPackage from "./external_package/rebundled_external_package"
+import RelativePathPair from "./source_file/relative_path_pair"
+import SourceDirectory from "./source_file/source_directory"
+import UnnamedSourceFile from "./source_file/unnamed_source_file"
+import interop from "./interop"
 
 export {
 	CommonInfoBuilder,
@@ -13,7 +14,8 @@ export {
 	LinkedExternalPackage,
 	NamedSourceFile,
 	RebundledExternalPackage,
+	RelativePathPair,
 	SourceDirectory,
 	UnnamedSourceFile,
 	interop
-};
+}
