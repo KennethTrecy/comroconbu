bbb415fe354fbca690c781af58e31a7b3c6aafa6
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-12-29 12:20:59 +0800

    internal(source): allow relative path pair for named source file

--- src/source_file/named_source_file.js
+++ src/source_file/named_source_file.js
@@ -1,6 +1,8 @@
-import AbstractExternalPackage from "../abstract_external_package";
-import CommonInfo from "../common_info";
-import UnnamedSourceFile from "./unnamed_source_file";
+// eslint-disable-next-line no-unused-vars
+import AbstractExternalPackage from "../abstract_external_package"
+// eslint-disable-next-line no-unused-vars
+import CommonInfo from "../common_info"
+import UnnamedSourceFile from "./unnamed_source_file"
 
 /**
  * Represents a source file that will be bundled.
@@ -10,22 +12,25 @@ export default class NamedSourceFile extends UnnamedSourceFile {
 	 * Creates a representation of source file.
 	 * @param {CommonInfo} commonInfo Common information for sources.
 	 * @param {string} name Name of the bundle.
-	 * @param {string} file The path of the source file to be bundled relative to the
-	 *                 `inputDirectory` of `CommonInfoBuilder` class.
+	 * @param {string|RelativePathPair} file The relative path of the source and bundled file which
+	 *                                       are both relative to the `inputDirectory` and
+	 *                                       `outputDirectory` of `CommonInfoBuilder` class. It can
+	 *                                       be also a relative path pair which may have two
+	 *                                       different directories.
 	 * @param {any} plugins Plugins that will be used to bundle the source file.
 	 * @param {AbstractExternalPackage[]} externals Optional. Array of external packages that will
 	 *                                    not be included in the bundle.
 	 */
 	constructor(commonInfo, name, file, plugins, externals = []) {
-		super(commonInfo, file, plugins, externals);
-		this._name = name;
+		super(commonInfo, file, plugins, externals)
+		this._name = name
 	}
 
 	_convertIntoBasicConfiguration() {
-		const configuration = super._convertIntoBasicConfiguration();
+		const configuration = super._convertIntoBasicConfiguration()
 
-		configuration.output.name = this._name;
+		configuration.output.name = this._name
 
-		return configuration;
+		return configuration
 	}
 }
