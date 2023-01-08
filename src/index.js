e8636abfbca26ea50e87ead225dd7182cc2af037
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-08 21:30:39 +0800

    feat: make common info structure to be accessible

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
+import CommonInfo from "./common_info"
 import CommonInfoBuilder from "./common_info_builder"
 import ImportedExternalPackage from "./external_package/imported_external_package"
 import LinkedExternalPackage from "./external_package/linked_external_package"
 import NamedSourceFile from "./source_file/named_source_file"
 import RebundledExternalPackage from "./external_package/rebundled_external_package"
 import RelativePathPair from "./source_file/relative_path_pair"
-import RelativePathPairBuilder from "./source_file/relative_path_pair_builder"
 import SourceDirectory from "./source_file/source_directory"
 import UnnamedSourceFile from "./source_file/unnamed_source_file"
 import interop from "./interop"
 
 export {
+	CommonInfo,
 	CommonInfoBuilder,
 	ImportedExternalPackage,
 	LinkedExternalPackage,
 	NamedSourceFile,
 	RebundledExternalPackage,
 	RelativePathPair,
-	RelativePathPairBuilder,
 	SourceDirectory,
 	UnnamedSourceFile,
 	interop
