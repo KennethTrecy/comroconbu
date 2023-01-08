d70fb1017d6d6a4302aadfc0b84a8fff491579ca
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-01-08 21:42:13 +0800

    chore: remove unused test

--- src/index.spec.js
+++
@@ -1,9 +0,0 @@
-var { expect } = require("chai");
-
-describe("Operation", function() {
-	describe("Addition", function() {
-		it("should return 2", function() {
-			expect(1+1).to.equal(2);
-		});
-	});
-});
